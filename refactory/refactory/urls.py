003dc582b792b06362ebd84d548e283046f77e14
LwangaConrad <lwangaconradarthur@gmail.com>
2024-04-11 08:37:17 +0300

    adding actual templates

--- refactory/refactory/urls.py
+++ refactory/refactory/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from helloworld import views
+from about import views as vw
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('about/', vw.aboutindex)
 ]
